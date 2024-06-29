64007c12ff53fbb6beab6ea47aadcbaab78aadb3
Eldahl <71466904+eldahl@users.noreply.github.com>
2024-06-29 15:59:36 +0200

    More configuration and home-manager

--- configuration.nix
+++ configuration.nix
@@ -2,13 +2,14 @@
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
-    ];
+	imports =
+		[ # Include the results of the hardware scan.
+      			./hardware-configuration.nix
+			inputs.home-manager.nixosModules.default
+    		];
 
   # Bootloader.
   boot.loader.grub.enable = true;
@@ -25,125 +26,188 @@
 
   boot.initrd.luks.devices."luks-daa6f869-d6f4-4090-a639-ccc2b0cc306c".keyFile = "/crypto_keyfile.bin";
   boot.initrd.luks.devices."luks-0d07dc1d-0df5-41cd-bcdf-64954493b2df".keyFile = "/crypto_keyfile.bin";
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  	
+	# Disable password prompt for sudo commands
+	security.sudo.extraRules= [{  
+		users = [ "ebber" ];
+    		commands = [
+       			{
+	 			command = "ALL" ;
+         			options= [ "NOPASSWD" ]; # "SETENV" # Adding the following could be a good idea
+      			}
+    		];
+  	}];
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+	home-manager = {
+		extraSpecialArgs = { inherit inputs; };
+		users = {
+			"ebber" = import ./home.nix;
+		};
+	};
 
-  # Enable networking
-  networking.networkmanager.enable = true;
+	networking.hostName = "desktop"; # Define your hostname.
+  	# networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
-  # Set your time zone.
-  time.timeZone = "Europe/Copenhagen";
+  	# Configure network proxy if necessary
+	# networking.proxy.default = "http://user:password@proxy:port/";
+	# networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_DK.UTF-8";
+	# Enable networking
+	networking.networkmanager.enable = true;
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "da_DK.UTF-8";
-    LC_IDENTIFICATION = "da_DK.UTF-8";
-    LC_MEASUREMENT = "da_DK.UTF-8";
-    LC_MONETARY = "da_DK.UTF-8";
-    LC_NAME = "da_DK.UTF-8";
-    LC_NUMERIC = "da_DK.UTF-8";
-    LC_PAPER = "da_DK.UTF-8";
-    LC_TELEPHONE = "da_DK.UTF-8";
-    LC_TIME = "da_DK.UTF-8";
-  };
+	# Set your time zone.
+	time.timeZone = "Europe/Copenhagen";
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
+	# Select internationalisation properties.
+	i18n.defaultLocale = "en_DK.UTF-8";
 
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+	i18n.extraLocaleSettings = {
+	LC_ADDRESS = "da_DK.UTF-8";
+	LC_IDENTIFICATION = "da_DK.UTF-8";
+	LC_MEASUREMENT = "da_DK.UTF-8";
+	LC_MONETARY = "da_DK.UTF-8";
+	LC_NAME = "da_DK.UTF-8";
+	LC_NUMERIC = "da_DK.UTF-8";
+	LC_PAPER = "da_DK.UTF-8";
+	LC_TELEPHONE = "da_DK.UTF-8";
+	LC_TIME = "da_DK.UTF-8";
+	};
 
-  # Configure keymap in X11
-  services.xserver = {
-    layout = "dk";
-    xkbVariant = "";
-  };
+	# Configure console keymap
+	console.keyMap = "dk-latin1";
 
-  # Configure console keymap
-  console.keyMap = "dk-latin1";
+	hardware.keyboard.zsa.enable = true;
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
+	# Enable CUPS to print documents.
+	services.printing.enable = true;
+	
+	# Enable the KDE Plasma Desktop Environment.
+	services.displayManager.sddm.enable = true;
+	services.desktopManager.plasma6.enable = true;
 
-  # Enable sound with pipewire.
-  hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+	# Configure keymap in X11
+	services.xserver = {
+		layout = "dk";
+		xkbVariant = "";
+	};
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+  	# Enable the X10 windowing system.
+  	# You can disable this if you're only using the Wayland session.
+  	services.xserver.enable = true;
+	
+	# NVIDIA Driver
+	hardware.opengl = {
+		enable = true;
+	};
+	services.xserver.videoDrivers = ["nvidia"];
+	hardware.nvidia = {
+		
+		modesetting.enable = true;
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+		powerManagement.enable = true;
+		powerManagement.finegrained = false;
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
-  users.users.ebber = {
-    isNormalUser = true;
-    description = "ebber";
-    extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
-  };
+		open = false;
 
-  # Install firefox.
-  programs.firefox.enable = true;
+		nvidiaSettings = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+		package = config.boot.kernelPackages.nvidiaPackages.stable;
+		#package = config.boot.kernelPackages.nvidiaPackages.beta;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  	vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  	wget
-	git
-	gh
-  ];
+	};
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+	# Enable sound with pipewire.
+	hardware.pulseaudio.enable = false;
+	security.rtkit.enable = true;
+	services.pipewire = {
+	enable = true;
+	alsa.enable = true;
+	alsa.support32Bit = true;
+	pulse.enable = true;
+	# If you want to use JACK applications, uncomment this
+	#jack.enable = true;
 
-  # List services that you want to enable:
+	# use the example session manager (no others are packaged yet so this is enabled by default,
+	# no need to redefine it in your config for now)
+	#media-session.enable = true;
+	};
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+	# Enable touchpad support (enabled default in most desktopManager).
+	# services.xserver.libinput.enable = true;
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+  	# Define a user account. Don't forget to set a password with ‘passwd’.
+	users.users.ebber = {
+		isNormalUser = true;
+		description = "ebber";
+		extraGroups = [ "networkmanager" "wheel" ];
+		packages = with pkgs; [
+			kdePackages.kate
+			thunderbird
+			
+			chromium
+			vscodium
+			
+			element-desktop
+			signal-desktop	
+			
+			spotify
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.05"; # Did you read the comment?
+			bitwarden-desktop
+			
+			obsidian
+			
+			wine64
+			winetricks
+			bottles
+		];
+	};
+
+  	# Install firefox.
+  	programs.firefox.enable = true;
+
+  	# Allow unfree packages
+  	nixpkgs.config.allowUnfree = true;
+
+  	# List packages installed in system profile. To search, run:
+  	# $ nix search wget
+  	environment.systemPackages = with pkgs; [
+		keymapp
+		zsa-udev-rules
+		vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  		wget
+		git
+		gh
+		htop
+		openvpn
+		vesktop
+	];
+
+	# Some programs need SUID wrappers, can be configured further or are
+	# started in user sessions.
+	# programs.mtr.enable = true;
+	# programs.gnupg.agent = {
+	#   enable = true;
+	#   enableSSHSupport = true;
+	# };
+
+	# List services that you want to enable:
+
+	# Enable the OpenSSH daemon.
+	# services.openssh.enable = true;
+
+	# Open ports in the firewall.
+	# networking.firewall.allowedTCPPorts = [ ... ];
+	# networking.firewall.allowedUDPPorts = [ ... ];
+	# Or disable the firewall altogether.
+	# networking.firewall.enable = false;
+
+	# This value determines the NixOS release from which the default
+	# settings for stateful data, like file locations and database versions
+	# on your system were taken. It‘s perfectly fine and recommended to leave
+	# this value at the release version of the first install of this system.
+	# Before changing this value read the documentation for this option
+	# (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  	system.stateVersion = "24.05"; # Did you read the comment?
   
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  	nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
--- flake.nix
+++ flake.nix
@@ -3,16 +3,25 @@
 
 	inputs = {
 		nixpkgs.url = "nixpkgs/nixos-24.05";
+
+		home-manager = {
+			url = "github:nix-community/home-manager/release-24.05";
+			inputs.nixpkgs.follows = "nixpkgs";
+		};
 	};
 
-	outputs = { self, nixpkgs, ... }: 
+	outputs = { self, nixpkgs, ... }@inputs: 
 		let 
 			lib = nixpkgs.lib;
 		in {
 		nixosConfigurations = {
 			desktop = lib.nixosSystem {
 				system = "x86_64-linux";
-				modules = [ ./configuration.nix ];
+				specialArgs = {inherit inputs;};
+				modules = [ 
+					./configuration.nix 
+					inputs.home-manager.nixosModules.default
+				];
 			};
 		};
 	};
