df6afed87cf0ee4a6a5b09759e6d9d1545732d05
Eldahl <71466904+eldahl@users.noreply.github.com>
2024-07-03 03:11:57 +0200

    more v4l2loopback

--- configuration.nix
+++ configuration.nix
@@ -30,6 +30,9 @@
 	# v4l2loopback is for virtual camera using OBS
 	# 
 	boot.kernelModules = [ "v4l2loopback" ];
+	boot.extraModulePackages = with config.boot.kernelPackages; [
+	  v4l2loopback
+	];
 	boot.extraModprobeConfig = ''
 	  options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
 	'';
