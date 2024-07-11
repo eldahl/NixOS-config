a14c3be4839e0a574380cca792e6271569eaef41
Eldahl <71466904+eldahl@users.noreply.github.com>
2024-07-11 02:10:38 +0200

    Xbox controller

--- configuration.nix
+++ configuration.nix
@@ -33,6 +33,7 @@
 	boot.kernelModules = [ "v4l2loopback" ];
 	boot.extraModulePackages = with config.boot.kernelPackages; [
 	  v4l2loopback
+	  xpadneo
 	];
 	boot.extraModprobeConfig = ''
 	  options v4l2loopback devices=1 video_nr=1 card_label="OBS Cam" exclusive_caps=1
